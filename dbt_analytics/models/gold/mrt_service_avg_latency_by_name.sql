4f034d93d1733ab1ed5219754b2f5511623267db
dartjomd <temadoron9@gmail.com>
2026-05-06 13:26:57 +0300

    ыва

--- dbt_analytics/models/gold/mrt_service_avg_latency_by_name.sql
+++ dbt_analytics/models/gold/mrt_service_avg_latency_by_name.sql
@@ -6,12 +6,13 @@
 ) }}
 
 select
-    service_name,
+    {# service_name,
     avg(latency_ms) as avg_latency_ms,
     count(*) as total_requests,
     -- audit columns
     max(_occurred_at) as _data_up_to_at,
-    {{ audit_columns() }}
+    {{ audit_columns() }} #}
+    *
 from {{ ref('fct_microservices_logs') }}
 group by service_name
 {{ is_run_limited() }}
