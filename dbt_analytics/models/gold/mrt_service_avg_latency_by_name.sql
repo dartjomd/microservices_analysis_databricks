b1f605de4eedd2da70e3b16bf4425f161b20fe02
dartjomd <temadoron9@gmail.com>
2026-05-06 13:20:18 +0300

    er

--- dbt_analytics/models/gold/mrt_service_avg_latency_by_name.sql
+++ dbt_analytics/models/gold/mrt_service_avg_latency_by_name.sql
@@ -9,8 +9,6 @@ select
     service_name,
     avg(latency_ms) as avg_latency_ms,
     count(*) as total_requests,
-    333 as sdfsdf,
-    333 as dop,
     -- audit columns
     max(_occurred_at) as _data_up_to_at,
     {{ audit_columns() }}
